8676e86aa0583fd34cd258278675eeae8649a000
Puja <waglepuja@gmail.com>
2021-02-10 22:37:35 -0500

    Updated server.js

--- website/app.js
+++ website/app.js
@@ -6,26 +6,34 @@
 
 
 
-// Function to POST data
+/* Function to POST data */
 const postData = async ( url = '', data = {})=>{
-    console.log(data);
+    console.log(data)
       const response = await fetch(url, {
-      method: 'POST', 
-      credentials: 'same-origin',
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      credentials: 'same-origin', // include, *same-origin, omit
       headers: {
           'Content-Type': 'application/json',
       },
-     // Body data type must match "Content-Type" header        
-      body: JSON.stringify(data), 
+      body: JSON.stringify(data), // body data type must match "Content-Type" header        
     });
-
+  
       try {
         const newData = await response.json();
-        console.log(newData);
-        return newData;
+        // console.log(newData);
+        return newData
       }catch(error) {
       console.log("error", error);
+      // appropriately handle the error
       }
   }
+  
 
-postData('/addMovie', {movie:' the matrix', score:5});
+postData('/addMovie', {movie:'the matrix', score:5});
+postData('/addMovie', {movie: 'Pitch Perfect', score:3} );
+
+
+// 9ea15014039e4036b1113881c7de76a0
+
+// https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid:${APIkey}&units=metric
+
