6458f64d979ea8c4378061a4cb395bfcf3b31325
Puja <waglepuja@gmail.com>
2021-02-11 14:00:53 -0500

    Async Fetch added to app.js

--- server.js
+++ server.js
@@ -1,64 +1,4 @@
 // // Setup empty JS object to act as endpoint for all routes
-// projectData = {};
-
-// // Require Express to run server and routes
-// const express = require('express');
-
-// // Start up an instance of app
-// const app = express();
-
-// /* Dependencies */
-// const bodyParser = require('body-parser');
-
-// /* Middleware*/
-// //Here we are configuring express to use body-parser as middle-ware.
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-
-// // Cors for cross origin allowance. 
-// // Cors is a package that lets the browser and server talk to each other without any security interruptions.
-// const cors = require('cors');
-// app.use(cors());
-
-// // Initialize the main project folder
-// app.use(express.static('website'));
-
-
-// // Setup Server
-// const port = 8000;
-
-// // Spin up the server
-// const server = app.listen(port, listening);
-
-
-// // Callback to debug. This function will run when we execute the listen method to let you know that the server is running and on which port by logging messages to the console. 
-// function listening(){
-//     console.log("server running");
-//     console.log(`running on localhost: ${port}`);
-// }
-
-
-// // Create a GET route that uses the url /all and returns the JavaScript object named projectData.
-// app.get('/all', sendData)
-
-// function sendData (request, response) {
-//     response.send(projectData);
-// }
-
-// // Create an array to hold data. Post Route
-// const data = [];
-// app.post('/add', addInfo );
-
-// function addInfo (req, res){
-//     projectData['date'] = req.body.date;
-//     projectData['temp'] = req.body.temp;
-//     projectData['date'] = req.body.date;    
-//     res.send(projectData);    
-//  }
-
-
-
-// Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
 // Require Express to run server and routes
@@ -75,48 +15,49 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Cors for cross origin allowance
+// Cors for cross origin allowance. 
+// Cors is a package that lets the browser and server talk to each other without any security interruptions.
 const cors = require('cors');
 app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
-const port = 8080;
 
-//Spin up the server
+
+// Setup Server
+const port = 8000;
+
+// Spin up the server
 const server = app.listen(port, listening);
 
-// Callback to debug
+
+// Callback to debug. This function will run when we execute the listen method to let you know that the server is running and on which port by logging messages to the console. 
 function listening(){
-    console.log('server running');
+    console.log("server running");
     console.log(`running on localhost: ${port}`);
-};
+}
+
+
+// Create a GET route that uses the url /all and returns the JavaScript object named projectData.
+app.get('/all', sendData)
+
+function sendData (request, response) {
+    response.send(projectData);
+}
 
 // Create an array to hold data. Post Route
 const data = [];
-app.post('/addMovie', addMovie )
+app.post('/add', addInfo );
 
-function addMovie (req, res){
-    data.push(req.body)
-    console.log(req.body)
-    res.send(data);     
+function addInfo (req, res){
+    projectData['date'] = req.body.date;
+    projectData['temp'] = req.body.temp;
+    projectData['feelings'] = req.body.feelings;    
+    res.send(projectData);    
  }
 
 
-//GET route that returns the projectData object
-// app.get('/all', sendData)
 
-// function sendData (request, response) {
-//     response.send(projectData)
-// }
 
-// POST route
-// app.post('/addWeatherData', addData)
 
-// function addData(request, response) {
-//     projectData.temperature = request.body.temperature;
-//     projectData.date = request.body.date;
-//     projectData.user_response = request.body.user_response;
-//     response.end();
-//     console.log(projectData)
-// }
+
--- website/app.js
+++ website/app.js
@@ -1,8 +1,35 @@
 /* Global Variables */
 
+// Base URL and API Key for OpenWeatherMap API
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+let apiKey = ' 9ea15014039e4036b1113881c7de76a0';
+
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', performAction);
+
+// Function called by event listener
+function performAction(e){
+  const newZip = document.getElementById('zip').value;
+  const content = document.getElementById('feelings').value;
+}
+
+/* Function to GET Web API Data*/
+const getTemperatureByZipCode = async (baseURL, newZip, apiKey) => {
+  // res equals to the result of fetch function
+  const res = await fetch(baseURL + newZip + apiKey);
+  try {
+    // userData equals to the result of fetch function
+    const userData = await res.json();
+    return userData;
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+
 // Create a new date instance dynamically with JS
-// let d = new Date();
-// let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 
 
@@ -26,14 +53,17 @@ const postData = async ( url = '', data = {})=>{
       console.log("error", error);
       // appropriately handle the error
       }
-  }
-  
+  } 
 
-postData('/addMovie', {movie:'the matrix', score:5});
-postData('/addMovie', {movie: 'Pitch Perfect', score:3} );
 
 
 // 9ea15014039e4036b1113881c7de76a0
 
 // https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid:${APIkey}&units=metric
+
+
+
+
+
+
 
